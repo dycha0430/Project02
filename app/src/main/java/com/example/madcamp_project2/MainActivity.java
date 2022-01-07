02d115cfc8d575d4755525dfd2001ea1c0ec1db4
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-01-07 16:33:31 +0900

    login version 1

--- app/src/main/java/com/example/madcamp_project2/MainActivity.java
+++ app/src/main/java/com/example/madcamp_project2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.madcamp_project2;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -15,6 +21,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.madcamp_project2.databinding.ActivityMainBinding;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +63,52 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        getHashKey();
+
+        Button login_btn = findViewById(R.id.kakao_login);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this,(oAuthToken, error) -> {
+                    if (error != null) {
+                        Log.e("LOGIN", "로그인 실패", error);
+                    } else if (oAuthToken != null) {
+                        get_user_info();
+                        Log.i("LOGIN", "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+                    }
+                    return null;
+                });
+            }
+        });
+
+        Button logout_btn = findViewById(R.id.kakao_logout);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserApiClient.getInstance().logout(error -> {
+                    if (error != null) {
+                        Log.e("LOGOUT", "로그아웃 실패, SDK에서 토큰 삭제됨", error);
+                    }else{
+                        Log.e("LOGOUT", "로그아웃 성공, SDK에서 토큰 삭제됨");
+                    }
+                    return null;
+                });
+            }
+        });
+    }
+
+    public void get_user_info() {
+        UserApiClient.getInstance().me((user, meError) -> {
+            if (meError != null) {
+                Log.e("USERINFO", "사용자 정보 요청 실패", meError);
+            } else {
+                Log.i("USERINFO", "사용자 정보 요청 성공" +
+                        "\n회원번호: "+user.getId() +
+                        "\n이메일: "+user.getKakaoAccount().getEmail());
+            }
+            return null;
+        });
     }
 
     @Override
@@ -61,5 +123,26 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
     }
 }
