bec3411a3123be59c65a43298fa35787ec88b850
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-01-11 20:42:45 +0900

    Add Member in Travel

--- app/src/main/java/com/example/madcamp_project2/ui/friends/Friend/SelectFriendAdapter.java
+++ app/src/main/java/com/example/madcamp_project2/ui/friends/Friend/SelectFriendAdapter.java
@@ -1,6 +1,7 @@
 package com.example.madcamp_project2.ui.friends.Friend;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.madcamp_project2.LoginActivity;
+import com.example.madcamp_project2.MainActivity;
+import com.example.madcamp_project2.MyAPI;
 import com.example.madcamp_project2.R;
 import com.example.madcamp_project2.ui.User;
 import com.example.madcamp_project2.ui.home.addtrip.AddExtraActivity;
+import com.example.madcamp_project2.ui.home.addtrip.Travel.TravelRequestSend;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SelectFriendAdapter extends RecyclerView.Adapter<SelectFriendAdapter.ViewHolder> {
     Context context;
@@ -48,6 +65,48 @@ public class SelectFriendAdapter extends RecyclerView.Adapter<SelectFriendAdapte
 
                 if (selected_position == -1) {
                     // TODO 해당 친구에게 요청 보내기
+                    String token = "";
+                    String email = "";
+
+                    String file_path = MainActivity.get_filepath();
+                    JSONParser parser = new JSONParser();
+
+                    try {
+                        FileReader reader = new FileReader(file_path+"/userinfo.json");
+                        Object obj = parser.parse(reader);
+                        JSONObject jsonObject = (JSONObject) obj;
+                        reader.close();
+
+                        token = jsonObject.get("token").toString();
+                        email = jsonObject.get("email").toString();
+                    }
+                    catch (IOException | ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    MyAPI myapi = LoginActivity.get_MyAPI();
+                    ArrayList<User> to_send = new ArrayList<>();
+                    to_send.add(user);
+                    TravelRequestSend travelRequestSend = new TravelRequestSend(email, to_send, tripPlan.getTravel_id());
+
+                    Call<TravelRequestSend> post_travel_request = myapi.post_travel_request("Bearer " + token, travelRequestSend);
+                    post_travel_request.enqueue(new Callback<TravelRequestSend>() {
+                        @Override
+                        public void onResponse(Call<TravelRequestSend> call, Response<TravelRequestSend> response) {
+                            if(response.isSuccessful()) {
+                                Log.d("REQUEST TO JOIN", "SUCCESS");
+                            }
+                            else {
+                                Log.d("REQUEST TO JOIN", "FAILED");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<TravelRequestSend> call, Throwable t) {
+                            Log.d("REQUEST TO JOIN", "FAILED");
+                        }
+                    });
+
                     bottomSheetDialogFragment.dismiss();
                     return;
                 }
