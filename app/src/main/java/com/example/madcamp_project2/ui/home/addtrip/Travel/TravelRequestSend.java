70b701c5034678dce663ec026b570329399aebd0
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-01-11 16:22:35 +0900

    Request Travel bug fixed

--- app/src/main/java/com/example/madcamp_project2/ui/home/addtrip/Travel/TravelRequestSend.java
+++ app/src/main/java/com/example/madcamp_project2/ui/home/addtrip/Travel/TravelRequestSend.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 
 public class TravelRequestSend {
     private String from_user_email;
-    private ArrayList<String> to_user_email;
+    private ArrayList<String> to_user_emails;
     private int travel_id;
 
     public TravelRequestSend(String from_user_email, ArrayList<User> selectedFriends, int travel_id) {
         this.from_user_email = from_user_email;
-        this.to_user_email = new ArrayList<>();
+        this.to_user_emails = new ArrayList<>();
         this.travel_id = travel_id;
 
         for(User friend : selectedFriends) {
-            this.to_user_email.add(friend.getEmail());
+            this.to_user_emails.add(friend.getEmail());
         }
     }
 }
